23473649640c7f5dadcf306be638c3b16146907e
Szymon <szymonwojcik800@gmail.com>
2024-06-19 14:41:52 +0200

    Feat: Redesign login form

--- PMan.App/PMan.App/Forms/Base/LoginForm.Designer.cs
+++ PMan.App/PMan.App/Forms/Base/LoginForm.Designer.cs
@@ -30,99 +30,360 @@ namespace StorageManager.App
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            button6 = new Button();
+            button7 = new Button();
+            label5 = new Label();
             label1 = new Label();
+            pictureBox1 = new PictureBox();
             label3 = new Label();
-            textBox3 = new TextBox();
+            panel1 = new Panel();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            checkBox2 = new CheckBox();
+            label2 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            panel4 = new Panel();
+            button3 = new Button();
             button2 = new Button();
-            checkBox1 = new CheckBox();
+            sqlConnection1 = new Microsoft.Data.SqlClient.SqlConnection();
+            panel5 = new Panel();
+            button4 = new Button();
+            button5 = new Button();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
             SuspendLayout();
             // 
-            // textBox1
+            // panel2
             // 
-            textBox1.Location = new Point(150, 58);
-            textBox1.MaximumSize = new Size(152, 23);
-            textBox1.MinimumSize = new Size(152, 23);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(152, 23);
-            textBox1.TabIndex = 0;
-            textBox1.KeyDown += textBox1_KeyDown;
+            panel2.BackColor = Color.FromArgb(41, 128, 185);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(300, 626);
+            panel2.TabIndex = 13;
+            panel2.MouseDown += panel2_MouseDown;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(button6);
+            panel3.Controls.Add(button7);
+            panel3.Location = new Point(3, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(297, 40);
+            panel3.TabIndex = 16;
+            panel3.MouseDown += panel3_MouseDown_1;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.FromArgb(41, 128, 185);
+            button6.Location = new Point(713, 0);
+            button6.Name = "button6";
+            button6.Size = new Size(40, 40);
+            button6.TabIndex = 14;
+            button6.Text = "_";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.FromArgb(41, 128, 185);
+            button7.Location = new Point(759, 0);
+            button7.Name = "button7";
+            button7.Size = new Size(40, 40);
+            button7.TabIndex = 13;
+            button7.Text = "X";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(66, 267);
+            label5.Name = "label5";
+            label5.Size = new Size(189, 30);
+            label5.TabIndex = 2;
+            label5.Text = "Workflow Manager";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(102, 61);
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(66, 224);
             label1.Name = "label1";
-            label1.Size = new Size(40, 15);
+            label1.Size = new Size(183, 30);
             label1.TabIndex = 1;
-            label1.Text = "Login:";
+            label1.Text = "Witaj w programie";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(93, 61);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(120, 120);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(102, 87);
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(317, 601);
             label3.Name = "label3";
-            label3.Size = new Size(40, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Hasło:";
+            label3.Size = new Size(126, 17);
+            label3.TabIndex = 2;
+            label3.Text = "Workflow manager";
             // 
-            // textBox3
+            // panel1
             // 
-            textBox3.Location = new Point(150, 87);
-            textBox3.Name = "textBox3";
-            textBox3.PasswordChar = '*';
-            textBox3.Size = new Size(152, 23);
-            textBox3.TabIndex = 5;
-            textBox3.KeyDown += textBox3_KeyDown;
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(checkBox2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox4);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(300, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(556, 626);
+            panel1.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(41, 128, 185);
+            label6.Location = new Point(43, 130);
+            label6.Name = "label6";
+            label6.Size = new Size(113, 30);
+            label6.TabIndex = 3;
+            label6.Text = "Zaloguj się";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(101, 178);
+            textBox2.MinimumSize = new Size(152, 23);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(413, 29);
+            textBox2.TabIndex = 0;
+            textBox2.KeyDown += textBox2_KeyDown;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 12F);
+            checkBox2.Location = new Point(101, 270);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(145, 25);
+            checkBox2.TabIndex = 12;
+            checkBox2.Text = "Zapamiętaj mnie";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(43, 181);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Login:";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(101, 301);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 36);
+            button1.TabIndex = 11;
+            button1.Text = "Zaloguj się";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.KeyDown += button1_KeyDown;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(43, 227);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 21);
+            label4.TabIndex = 4;
+            label4.Text = "Hasło:";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F);
+            textBox4.Location = new Point(101, 224);
+            textBox4.Name = "textBox4";
+            textBox4.PasswordChar = '*';
+            textBox4.Size = new Size(413, 29);
+            textBox4.TabIndex = 5;
+            textBox4.KeyDown += textBox4_KeyDown;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Transparent;
+            panel4.Controls.Add(button3);
+            panel4.Controls.Add(button2);
+            panel4.Location = new Point(300, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(701, 40);
+            panel4.TabIndex = 15;
+            panel4.MouseDown += panel4_MouseDown;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.FromArgb(41, 128, 185);
+            button3.Location = new Point(474, -1);
+            button3.Name = "button3";
+            button3.Size = new Size(40, 40);
+            button3.TabIndex = 14;
+            button3.Text = "_";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(150, 146);
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(41, 128, 185);
+            button2.Location = new Point(517, 0);
             button2.Name = "button2";
-            button2.Size = new Size(152, 26);
-            button2.TabIndex = 11;
-            button2.Text = "Zaloguj się";
+            button2.Size = new Size(40, 40);
+            button2.TabIndex = 13;
+            button2.Text = "X";
             button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            button2.Click += button2_Click_1;
             // 
-            // checkBox1
+            // sqlConnection1
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(150, 121);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(115, 19);
-            checkBox1.TabIndex = 12;
-            checkBox1.Text = "Zapamiętaj mnie";
-            checkBox1.UseVisualStyleBackColor = true;
+            sqlConnection1.AccessTokenCallback = null;
+            sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Transparent;
+            panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Controls.Add(panel4);
+            panel5.Controls.Add(panel1);
+            panel5.Controls.Add(panel2);
+            panel5.Controls.Add(button4);
+            panel5.Controls.Add(button5);
+            panel5.Dock = DockStyle.Fill;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(858, 628);
+            panel5.TabIndex = 16;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.FromArgb(41, 128, 185);
+            button4.Location = new Point(1275, 0);
+            button4.Name = "button4";
+            button4.Size = new Size(40, 40);
+            button4.TabIndex = 14;
+            button4.Text = "_";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.FromArgb(41, 128, 185);
+            button5.Location = new Point(1321, 0);
+            button5.Name = "button5";
+            button5.Size = new Size(40, 40);
+            button5.TabIndex = 13;
+            button5.Text = "X";
+            button5.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(425, 278);
-            Controls.Add(checkBox1);
-            Controls.Add(button2);
-            Controls.Add(textBox3);
-            Controls.Add(label3);
-            Controls.Add(label1);
-            Controls.Add(textBox1);
-            MaximumSize = new Size(441, 317);
+            ClientSize = new Size(858, 628);
+            ControlBox = false;
+            Controls.Add(panel5);
+            FormBorderStyle = FormBorderStyle.None;
             MinimumSize = new Size(441, 317);
             Name = "LoginForm";
-            StartPosition = FormStartPosition.CenterParent;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Zaloguj się";
+            Load += LoginForm_Load;
             KeyDown += LoginForm_KeyDown;
+            MouseDown += LoginForm_MouseDown;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private TextBox textBox1;
-        private Label label1;
+        private Panel panel2;
+        private Panel panel1;
+        private TextBox textBox2;
+        private CheckBox checkBox2;
+        private Label label2;
+        private Button button1;
+        private Label label4;
+        private TextBox textBox4;
+        private PictureBox pictureBox1;
         private Label label3;
-        private TextBox textBox3;
+        private Label label1;
+        private Label label5;
         private Button button2;
-        private CheckBox checkBox1;
+        private Label label6;
+        private Microsoft.Data.SqlClient.SqlConnection sqlConnection1;
+        private Panel panel4;
+        private Button button3;
+        private Panel panel5;
+        private Button button4;
+        private Button button5;
+        private Panel panel3;
+        private Button button6;
+        private Button button7;
     }
 }
--- PMan.App/PMan.App/Forms/Base/LoginForm.cs
+++ PMan.App/PMan.App/Forms/Base/LoginForm.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
 using Microsoft.VisualBasic.Logging;
 using StorageManager.App.Database;
 using StorageManager.App.Features.Users;
@@ -16,17 +18,41 @@ namespace StorageManager.App
 {
     public partial class LoginForm : Form
     {
+
+
+        private System.Windows.Forms.Timer _startFormTimer = new System.Windows.Forms.Timer();
+        private System.Windows.Forms.Timer _closeFormTimer = new System.Windows.Forms.Timer();
+        private System.Windows.Forms.Timer _closeDialogFormTimer = new System.Windows.Forms.Timer();
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         protected LoginForm()
         {
 
             InitializeComponent();
-            this.button2.Focus();
+            this.button1.Focus();
+
+            this.Opacity = 0;
+
+            this._closeFormTimer.Tick += new EventHandler(decreaseTimerOpacity);
+
+            this._closeFormTimer.Interval = 25;
+            this._startFormTimer.Interval = 25;
+            this._startFormTimer.Tick += new EventHandler(increaseTimerOpacity);
+            this._startFormTimer.Start();
         }
 
-        public static bool Open(bool tryToLoginFromRegistryCreds=false)
+        public static bool Open(bool tryToLoginFromRegistryCreds = false)
         {
             var login = RegistryAccessor.GetValue("UserLogin");
-            if(tryToLoginFromRegistryCreds)
+            if (tryToLoginFromRegistryCreds)
             {
                 var password = RegistryAccessor.GetValue("UserPwd", true);
                 var result = AppManager.Instance.Resolve<IUserService>().Login(login, password);
@@ -35,7 +61,8 @@ namespace StorageManager.App
                     return true;
             }
             var form = new LoginForm();
-            form.textBox1.Text = login;
+            form.textBox2.Text = login;
+
             return form.ShowDialog() == DialogResult.OK;
         }
 
@@ -46,19 +73,33 @@ namespace StorageManager.App
 
         private void PerformLogin()
         {
-            var login = textBox1.Text;
-            var password = textBox3.Text;
+            var login = textBox2.Text;
+            var password = textBox4.Text;
 
             var result = AppManager.Instance.Resolve<IUserService>().Login(login, password);
 
             if (result.IsSuccess)
             {
-                if(checkBox1.Checked)
+                if (checkBox2.Checked)
                 {
                     RegistryAccessor.SaveValue("UserLogin", login);
                     RegistryAccessor.SaveValue("UserPwd", password, true);
                 }
-                DialogResult = DialogResult.OK;
+
+                _closeDialogFormTimer.Interval = 25;
+                _closeDialogFormTimer.Tick += (object sender, EventArgs e) =>
+                {
+                    if (this.Opacity > 0)
+                    {
+                        this.Opacity -= 0.05;
+                    }
+                    else
+                    {
+                        this._startFormTimer.Stop();
+                        DialogResult = DialogResult.OK;
+                    }
+                };
+                _closeDialogFormTimer.Start(); 
             }
             else
             {
@@ -87,6 +128,119 @@ namespace StorageManager.App
             if (e.KeyCode == Keys.Enter)
             {
                 PerformLogin();
+            }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            Version versionInfo = Assembly.GetExecutingAssembly().GetName().Version;
+            DateTime startDate = new DateTime(2000, 1, 1);
+            int diffDays = versionInfo.Build;
+            DateTime computedDate = startDate.AddDays(diffDays);
+            string lastBuilt = computedDate.ToShortDateString();
+            this.label3.Text = string.Format("{0} - Wersja {1}",
+                        this.label3.Text, versionInfo.ToString(), lastBuilt);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            _closeFormTimer.Start();
+        }
+
+        private void increaseTimerOpacity(object sender, EventArgs e)
+        {
+            if (this.Opacity < 1)
+            {
+                this.Opacity += 0.05;
+            }
+            else
+            {
+                this._startFormTimer.Stop();
+            }
+        }
+
+        private void decreaseTimerOpacity(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+            {
+                this.Opacity -= 0.05;
+            }
+            else
+            {
+                this._startFormTimer.Stop();
+                Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PerformLogin();
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void LoginForm_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void panel4_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                PerformLogin();
+            }
+        }
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                PerformLogin();
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                PerformLogin();
+            }
+        }
+
+        private void panel3_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
         }
     }
